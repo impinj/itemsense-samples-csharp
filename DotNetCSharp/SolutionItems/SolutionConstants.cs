b33e2a92e900eadb438d4763391a575be5569843
Gary Mixson <gmixson@impinj.com>
2018-11-07 09:15:08 -0800

    add the solutionconstants file back

---
+++ DotNetCSharp/SolutionItems/SolutionConstants.cs
@@ -0,0 +1,15 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+//    Solution Constants
+//
+////////////////////////////////////////////////////////////////////////////////
+
+namespace DotNetExamples
+{
+    public static class SolutionConstants
+    {
+        public const string ItemSenseUri = "http://host.example.com/itemsense";
+        public const string ItemSenseUsername = "admin";
+        public const string ItemSensePassword = "admindefault";
+    }
+}
